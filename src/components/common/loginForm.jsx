c4e0e3432b40e44d1ec811820d9574c5eed6ed9f
Muhammed Mujahid <fakharm@hotmail.com>
2018-11-15 19:45:44 -0500

    extracted from inputs into a reusable stateless functional component to reduce duplication

--- src/components/common/loginForm.jsx
+++ src/components/common/loginForm.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
+import Input from "./input";
 
 class LoginForm extends Component {
+  state = {
+    account: { username: "", password: "" }
+  };
+  username = React.createRef();
   handleSubmit = e => {
     e.preventDefault();
+    const username = this.username.current.value;
     console.log("yoyoyo");
+  };
+  handlChange = e => {
+    const account = { ...this.state.account };
+    account.username = e.currentTarget.value;
+    this.setState({ account });
   };
 
   render() {
@@ -11,14 +22,18 @@ class LoginForm extends Component {
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="username">Userame</label>
-            <input id="username" type="text" className="form-control" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
-          </div>
+          <Input
+            name="username"
+            value={account.username}
+            label="Username"
+            onChange={this.handlChange}
+          />
+          <Input
+            name="password"
+            value={account.password}
+            label="Password"
+            onChange={this.handlChange}
+          />
           <button className="btn btn-primary">Login</button>
         </form>
       </div>
