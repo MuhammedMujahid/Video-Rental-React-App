516eb459101397df3f47271b790b987212c6248d
Muhammed Mujahid <fakharm@hotmail.com>
2018-11-16 19:08:34 -0500

    login button will be disabled until user enters login credentials

--- src/components/common/loginForm.jsx
+++ src/components/common/loginForm.jsx
@@ -18,13 +18,20 @@ class LoginForm extends Component {
   };
 
   validate = () => {
-    const optins = { abortEarly: false };
+    const options = { abortEarly: false };
     const { error } = Joi.validate(this.state.account, this.schema, options);
     if (!error) return null;
 
     const errors = {};
     for (let item of error.details) errors[item.path[0]] = item.message;
     return errors;
+  };
+
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
   };
 
   handleSubmit = e => {
@@ -76,7 +83,9 @@ class LoginForm extends Component {
             onChange={this.handleChange}
             error={errors.password}
           />
-          <button className="btn btn-primary">Login</button>
+          <button disabled={this.validate()} className="btn btn-primary">
+            Login
+          </button>
         </form>
       </div>
     );
